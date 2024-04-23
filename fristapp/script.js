b76819d95a9d8004cc2f32c7e97998fc417315cc
Brenno2003 <brennomachado02gmail.com>
2024-04-22 22:09:01 -0300

    Função de salario

--- fristapp/script.js
+++ fristapp/script.js
@@ -1,8 +1,26 @@
-ano = 2003;
+ano = parseInt(prompt("Informe o seu ano de nascimento: "))
+nome = prompt("Qual o seu nome: ")
+salarioBase = parseFloat(prompt("Informe o seu salario: "));
+gratificacao = parseFloat(prompt("Informe o sua gratificação: "));
+bonus = parseFloat(prompt("Informe o seu bonus: "));
+desconto = parseFloat(prompt("Informe o seu desconto: "));
+adicional = 0
+
 today = new Date();
 year = today.getFullYear();
 idade = year - ano;
-nome = "Brenno";
-mensagem = "Sou " + nome + " tenho " + idade + " anos";
+
+if (idade > 20){
+	adicional = 2000;
+}
+else{
+	adicional = 1000;
+}
+
+
+salarioLiquido = salarioBase + gratificacao + bonus - desconto;
+salarioLiquido += adicional;
+
+mensagem = "Sou " + nome + " tenho " + idade + " anos e ganho R$ " + salarioLiquido;
 alert(mensagem);
 
